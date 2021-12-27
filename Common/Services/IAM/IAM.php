adccf5517b568fe0b58cd28379320ee90e53a9a9
Kajna <milos@caenazzo.com>
2021-12-27 15:31:06 +0100

    IAM update

--- Common/Services/IAM/IAM.php
+++ Common/Services/IAM/IAM.php
@@ -5,6 +5,13 @@ namespace Common\Services\IAM;
 use Common\Services\IAM\Interfaces\IAMInterface;
 use Core\Container\ContainerAware;
 
+/**
+ * Class IAM
+ * Class assumes that container variable will hold 'user' variable with 'Contact' key that respects following structure:
+ * ['ContactID' => 'int', 'CompanyID' => 'int|null', 'Email' => 'string', 'ArchivedDate' => 'string|null', 'role_ids' => 'array[int]', '' => '
+ *
+ * @package Common\Services\IAM
+ */
 class IAM extends ContainerAware implements IAMInterface
 {
     public function __construct($container)
@@ -12,9 +19,17 @@ class IAM extends ContainerAware implements IAMInterface
         $this->container = $container;
     }
 
-    public function checkPermission($resource, $permission)
+    /**
+     * Function assumes there is a permissions map in the container.
+     * Map must be in the following structure [ ['resource_name' => 1] ] where 1 denotes permission level.
+     * @param string $resource
+     * @param int $permission
+     * @return bool
+     */
+    public function checkPermission(string $resource, int $permission): bool
     {
-        // TODO
+        $perms = $this->container['permissions'];
+        return $this->permissionCheck($perms[$resource], $permission);
     }
 
     public function getCompanyID(): ?int
@@ -42,5 +57,46 @@ class IAM extends ContainerAware implements IAMInterface
             $value = $this->user['Contact']['Email'];
         } catch (\Exception $e) {}
         return $value;
+    }
+
+    /**
+     * Checks if user have permission to execute passed action.
+     * PERMISSION_DENIED 0
+     * PERMISSION_CREATE 2
+     * PERMISSION_READ 1
+     * PERMISSION_UPDATE 4
+     * PERMISSION_DELETE 8
+     * @param int $userLevel
+     * @param int $permission
+     * @return bool
+     */
+    public function permissionCheck(int $userLevel, int $permission): bool
+    {
+        $accessRights = $this->bitMask($userLevel);
+        return in_array($permission, $accessRights);
+    }
+
+    /**
+     * Correct the variables stored in array.
+     *
+     * @param integer
+     * @return array
+     */
+    private function bitMask($mask = 0): array
+    {
+        if (!is_numeric($mask)) {
+            return [];
+        }
+        $return = [];
+        while ($mask > 0) {
+            $end = null;
+            for ($i = 0, $n = 0; $i <= $mask; $i = 1 * pow(2, $n), $n++) {
+                $end = $i;
+            }
+            $return[] = $end;
+            $mask = $mask - $end;
+        }
+        sort($return);
+        return $return;
     }
 }
