beffd2ecf27aa15d35f7030bcedbf98f54972875
Activ8 <activ8@Activ8s-MacBook-Pro.local>
2022-05-25 14:16:30 +0200

    SQS

--- Common/Services/AWS/SQS.php
+++ Common/Services/AWS/SQS.php
@@ -2,14 +2,17 @@
 
 namespace Common\Services\AWS;
 
+use Aws\Result;
 use Aws\Sqs\SqsClient;
 use Aws\Exception\AwsException;
+use Monolog\Logger;
 
 class SQS
 {
     private ?SqsClient $client = null;
+    private Logger $logger;
 
-    public function __construct($config)
+    public function __construct($config, $logger)
     {
         $this->client = new SqsClient([
             'region' => $config['region'],
@@ -19,20 +22,43 @@ class SQS
                 'secret' => $config['secret'],
             ]
         ]);
+        $this->logger = $logger;
     }
 
-    /**
-     * @param string $key
-     * @param string $filepath
-     * @param string $bucket
-     * @return Result
-     */
-    public function put(string $key, string $filepath, $bucket = "default_bucket")
+    public function send(string $QueueUrl, string $MessageBody, array $MessageAttributes = [], int $delay = 3): ?Result
     {
-        return $this->client->putObject([
-            'Bucket' => $bucket,
-            'Key' => $key,
-            'SourceFile' => $filepath
-        ]);
+        /*
+         * [
+                "Title" => [
+                    'DataType' => "String",
+                    'StringValue' => "The Hitchhiker's Guide to the Galaxy"
+                ],
+                "Author" => [
+                    'DataType' => "String",
+                    'StringValue' => "Douglas Adams."
+                ],
+                "WeeksOn" => [
+                    'DataType' => "Number",
+                    'StringValue' => "6"
+                ]
+            ]
+         */
+        $params = [
+            'DelaySeconds' => $delay,
+            'MessageAttributes' => $MessageAttributes,
+            'MessageBody' => $MessageBody,
+            'QueueUrl' => $QueueUrl
+        ];
+
+        try {
+            $result = $this->client->sendMessage($params);
+            $this->logger->error(SQS::class, $params);
+            return $result;
+        } catch (AwsException $e) {
+            $this->logger->error(SQS::class, [
+                'message' => $e->getMessage()
+            ]);
+            return null;
+        }
     }
 }
