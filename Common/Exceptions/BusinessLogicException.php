7fb1dcde0a2da3cd030ea174a8ab355a0bdecd94
Kajna <milos@caenazzo.com>
2024-06-07 12:24:07 +0200

    Update

---
+++ Common/Exceptions/BusinessLogicException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Common\Exceptions;
+
+class BusinessLogicException extends \Exception
+{
+
+}
--- Common/GeoLocationTrait.php
+++ Common/GeoLocationTrait.php
@@ -182,4 +182,30 @@ trait GeoLocationTrait
         }
         return 'unknown';
     }
+
+    /**
+     * Calculates the distance in miles between two points specified by latitude and longitude.
+     *
+     * @param float $lat1 Latitude of the first point
+     * @param float $lon1 Longitude of the first point
+     * @param float $lat2 Latitude of the second point
+     * @param float $lon2 Longitude of the second point
+     * @return float Distance in miles
+     */
+    public function calculateDistanceFromLatLon(float $lat1, float $lon1, float $lat2, float $lon2): float
+    {
+        $earthRadius = 3959.0; // Radius of the earth in miles
+
+        $dLat = deg2rad($lat2 - $lat1);  // Convert degrees to radians
+        $dLon = deg2rad($lon2 - $lon1);  // Convert degrees to radians
+
+        $a = sin($dLat/2) * sin($dLat/2) +
+            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+            sin($dLon/2) * sin($dLon/2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+        // Distance in miles
+
+        return $earthRadius * $c;
+    }
 }
