29d4144ae21320d14b0e48903d184316219413b5
Kajna <milos@caenazzo.com>
2024-11-05 11:57:40 +0100

    Update

--- Common/Exceptions/BusinessLogicException.php
+++ Common/Exceptions/BusinessLogicException.php
@@ -4,5 +4,15 @@ namespace Common\Exceptions;
 
 class BusinessLogicException extends \Exception
 {
+    private array $data = [];
 
+    public function __construct($message = "", $data = []) {
+        parent::__construct($message);
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
 }
